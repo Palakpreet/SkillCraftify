d15539f56cc3ba027de5f0e14f735a6847f4a146
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2024-02-13 02:51:55 +0530

    Frontend Setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import { Button} from 'antd';
+import './stylesheets/theme.css';
+import './stylesheets/alignments.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-screen bg-primary"></div>
+    </div>
+  );
+}
+
+export default App;
